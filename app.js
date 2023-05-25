c05d8f2c7fd4324e3dabd611943eb857441f2781
Soumyadip1245 <97156552+Soumyadip1245@users.noreply.github.com>
2023-05-25 09:59:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 var flutter = require('./routes/flutterproject')
-var nflutter = require('./routes/fluttergrocery')
 var PORT = process.env.port || 8080
 var app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/flutter', flutter)
-app.use('/f', nflutter)
 app.listen(PORT, () => {
     console.log(`Port: ${PORT}`)
 })
